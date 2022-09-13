ce6c19ff68862d76f068f58fb8f823b2fd338dfd
Moballighul islam <moballigh5225@gmail.com>
2022-09-13 22:19:53 +0530

    created file structure

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+//creating a index file for exporting all the components in one file
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetail } from "./VideoDetail";
